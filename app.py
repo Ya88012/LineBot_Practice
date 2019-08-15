2edf730ab6a51daacf1e110ef776210af450e11b
Ya88012 <Yun.Ye.8426@gmail.com>
2019-08-15 13:32:54 +0800

    第6次提交

--- app.py
+++ app.py
@@ -39,12 +39,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=event.message.text))
     word = event.message.text
     if word == "你好":
-        message = TextSendMessgae(text="Hello#")
+        message = TextSendMessgae(text="Hello#你好")
     else:
         message = TextSendMessage(text="抱歉我不了解#")
     if word == "幹":
