23dea848cd8b0757f07e0c0d3833040904ce2dd1
Ya88012 <Yun.Ye.8426@gmail.com>
2019-08-26 10:20:16 +0800

    Ver.69

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+global players_amount
+players_amount = 0
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import random
@@ -62,6 +65,7 @@ def handle_message(event):
         WorkSheet_Game.update_cell(100, 20, event.source.group_id)
 
     if word == "#準備完成":
+        global players_amount
         Temp = []
         Temp.append(str(event.source.user_id))
         profile = line_bot_api.get_profile(event.source.user_id)
@@ -69,9 +73,10 @@ def handle_message(event):
         Temp.append(player_name)       
         WorkSheet_Game.append_row(Temp)
         message = TextSendMessage(text="玩家 {} 已登入遊戲~~~".format(player_name))
+        players_amount += 1
 
     if word == "#遊戲開始":
-        players_amount = int(WorkSheet_Game.cell(100, 19).value)
+        global players_amount
         if players_amount == 8:
             message = [TextMessage(text="GameStart~~~"), TextMessage(text="本次遊戲共 {} 人遊玩".format(players_amount))]
             speciallist = random.sample(range(1, players_amount+1), 4)
